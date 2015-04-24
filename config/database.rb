7d4be82007e701ac935edf561b4131e0a6be39ba
ewolinetz <ewolinet@redhat.com>
2015-04-24 13:08:18 -0500

    adding some checking and logging for connection checking

--- config/database.rb
+++ config/database.rb
@@ -31,26 +31,13 @@ end
 
 def self.check_for_tables_prod
   begin
-    config = {
-      :adapter  => "postgresql",
-      :host     => "#{ENV["DATABASE_SERVICE_HOST"]}",
-      :port     => "#{ENV["DATABASE_SERVICE_PORT"]}",
-      :database => "#{ENV["POSTGRESQL_DATABASE"]}"
-    }
-    if ENV.key?("POSTGRESQL_ROOT_PASSWORD")
-      config[:password] = "#{ENV["POSTGRESQL_ROOT_PASSWORD"]}"
-    else
-      config[:username] = "#{ENV["POSTGRESQL_USER"]}"
-      config[:password] = "#{ENV["POSTGRESQL_PASSWORD"]}"
+
+    puts "Checking if connection is active..." 
+
+    if ActiveRecord::Base.connection.active?
+      puts "Checking for existance of table (key_pairs)..."
+      ActiveRecord::Base.connection.table_exists? 'key_pairs'
     end
-
-    puts "Connecting to production database (#{config[:username]}@#{config[:host]}:#{config[:port]})..."
-    ActiveRecord::Base.establish_connection(config)
-
-    ActiveRecord::Base.connection.active?
-
-    puts "Checking for existance of table (key_pairs)..."
-    ActiveRecord::Base.connection.table_exists? 'key_pairs'
 
   rescue Exception => e
     if not /Can't connect to POSTGRESQL server/ =~ e.message
@@ -63,7 +50,11 @@ end
 def self.close_connections
   begin
 
-  ActiveRecord::Base.finish
+  puts "Checking if connection is active..."
+  if ActiveRecord::Base.connection.active?
+    puts "Closing connection..."
+    ActiveRecord::Base.finish
+  end
 
   rescue Exception => e
     if not /Can't close connection to POSTGRESQL server/ =~ e.message
